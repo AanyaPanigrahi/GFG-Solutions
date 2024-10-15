1a25d8eb2c47d54c7297b96b402d2bd781619e17
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2024-10-15 23:11:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subarray range with given sum/subarray-range-with-given-sum.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count the number of subarrays which adds to the given sum.
+    int subArraySum(vector<int>& arr, int tar) {
+        // Your code here
+        unordered_map<int, int> sumFreq;
+    int currSum = 0, count = 0;
+    sumFreq[0] = 1;
+
+    for (int num : arr) {
+        currSum += num;
+        if (sumFreq.find(currSum - tar) != sumFreq.end()) {
+            count += sumFreq[currSum - tar];
+        }
+        sumFreq[currSum]++;
+    }
+
+    return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int tar = stoi(ks);
+        Solution obj;
+        int res = obj.subArraySum(arr, tar);
+        cout << res << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
