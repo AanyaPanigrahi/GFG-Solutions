1c1aba7789a90df0ff955454bebaeac6dd481bd6
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2024-12-18 22:38:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+  bool isPossible(vector<int>& arr, int k, int maxPages) {
+    int studentCount = 1, pagesSum = 0;
+    for (int pages : arr) {
+        if (pages > maxPages)
+            return false; 
+        if (pagesSum + pages > maxPages) {
+            studentCount++;
+            pagesSum = pages;
+            if (studentCount > k)
+                return false; 
+        } else {
+            pagesSum += pages;
+        }
+    }
+    return true;
+}
+
+    int findPages(vector<int> &arr, int k) {
+        // code here
+        if (arr.size() < k)
+        return -1; 
+
+    int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+    int result = -1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (isPossible(arr, k, mid)) {
+            result = mid;
+            high = mid - 1; 
+        } else {
+            low = mid + 1; 
+        }
+    }
+    return result;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.findPages(arr, d);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
